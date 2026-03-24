a5650b6a872c935d7687a4671c167d309c8a53b2
Dennis Eichhorn <spl1nes.com@googlemail.com>
2026-03-24 21:38:08 +0100

    update

--- archive_builder.cpp
+++ archive_builder.cpp
@@ -11,15 +11,15 @@
 #define DEBUG 0
 
 #include "../cOMS/system/FileUtils.cpp"
-#include "../cOMS/ui/UITheme.h"
+#include "../cOMS/ui/UITheme.cpp"
 #include "../cOMS/utils/StringUtils.h"
-#include "../cOMS/asset/AssetArchive.h"
+#include "../cOMS/asset/AssetArchive.cpp"
 #include "../cOMS/audio/Audio.cpp"
 #include "../cOMS/audio/Qoa.h"
 #include "../cOMS/image/Image.cpp"
 #include "../cOMS/image/Qoi.h"
-#include "../cOMS/object/Mesh.h"
-#include "../cOMS/localization/Language.h"
+#include "../cOMS/object/Mesh.cpp"
+#include "../cOMS/localization/Language.cpp"
 #include "../cOMS/gpuapi/opengl/ShaderUtils.h"
 #include "../cOMS/gpuapi/direct3d/ShaderUtils.h"
 
@@ -57,7 +57,7 @@ void build_enum(RingMemory* memory_volatile, FileBody* toc, char* output_path, i
     char namespace_name[64];
 
     strcpy(output_name, last_dir);
-    output_name[0] = toupper_ascii(*output_name);
+    output_name[0] = (char) toupper(*output_name);
 
     last_dir = output_name;
     while(*last_dir) {
@@ -70,9 +70,9 @@ void build_enum(RingMemory* memory_volatile, FileBody* toc, char* output_path, i
     }
 
     strcpy(namespace_name, output_name);
-    toupper_ascii(namespace_name);
+    str_toupper(namespace_name);
 
-    byte* enum_file_data = ring_get_memory(memory_volatile, 1 * MEGABYTE, 4);
+    byte* enum_file_data = ring_memory_get(memory_volatile, 1 * MEGABYTE, sizeof(size_t));
 
     sprintf((char *) enum_file_data,
         "/**\n"
@@ -136,7 +136,7 @@ void build_enum(RingMemory* memory_volatile, FileBody* toc, char* output_path, i
         int32 j = 0;
         for (int32 c = 0; file_name[c] != '\0'; ++c) {
             if (isalnum(file_name[c])) {
-                enum_name[j++] = toupper_ascii(file_name[c]);
+                enum_name[j++] = (char) toupper(file_name[c]);
             } else {
                 enum_name[j++] = '_';
             }
@@ -144,7 +144,7 @@ void build_enum(RingMemory* memory_volatile, FileBody* toc, char* output_path, i
         enum_name[j] = '\0';
 
         // Add the entry to the enum file content
-        sprintf((char *) enum_file_data + str_length((char *) enum_file_data), "    AA_ID_%s = %d | (%d << 24),\n", enum_name, count, toc_id);
+        sprintf((char *) enum_file_data + strlen((char *) enum_file_data), "    AA_ID_%s = %d | (%d << 24),\n", enum_name, count, toc_id);
         ++count;
 
         while (toc->content[i] != '\0') {
@@ -152,14 +152,14 @@ void build_enum(RingMemory* memory_volatile, FileBody* toc, char* output_path, i
         }
     }
 
-    sprintf((char *) enum_file_data + str_length((char *) enum_file_data),
+    sprintf((char *) enum_file_data + strlen((char *) enum_file_data),
         "};\n\n"
         "#endif\n"
     );
 
-    FileBody enum_file = {};
+    FileBody enum_file = {0};
     enum_file.content = enum_file_data;
-    enum_file.size = str_length((char *) enum_file_data);
+    enum_file.size = strlen((char *) enum_file_data);
 
     char* dot = strrchr(output_path, '.');
     if (dot != NULL) {
@@ -173,39 +173,49 @@ void build_enum(RingMemory* memory_volatile, FileBody* toc, char* output_path, i
 
 void build_asset_archive(RingMemory* memory_volatile, char* argv[], const char* rel_path, FileBody* toc)
 {
-    BufferMemory memory_output = {};
+    BufferMemory memory_output = {0};
     memory_output.memory = (byte *) malloc(sizeof(byte) * GIGABYTE * 4);
     memory_output.head = memory_output.memory;
     memory_output.size = sizeof(byte) * GIGABYTE * 4;
     memory_output.end = memory_output.memory + memory_output.size;
 
-    // Output memory ranges
-    byte* archive_header = buffer_get_memory(&memory_output, 4 * MEGABYTE, 4);
-    FileBody output_header = {};
+    // Reserver output memory ranges
+    byte* archive_header = buffer_memory_get(&memory_output, 4 * MEGABYTE, sizeof(size_t));
+    FileBody output_header = {0};
     output_header.content = archive_header;
 
+    // We don't know how large our header actually needs to be
+    // We just pick a generous offset.
+    // Don't worry we will not store empty space in our archive file
     byte* archive_body = archive_header + 4 * MEGABYTE;
-    FileBody output_body = {};
+    FileBody output_body = {0};
     output_body.content = archive_body;
 
+    // Write archive version to header
     *((int32 *) archive_header) = SWAP_ENDIAN_LITTLE(ASSET_ARCHIVE_VERSION);
     archive_header += sizeof(int32);
 
+    // "Write" asset count to header
+    // We actually only create the placeholder since we don't know the asset count just yet
+    // For that reason we create a pointer to that location so we can easily write the actual count later on
     *((uint32 *) archive_header) = 0;
     uint32* asset_count = (uint32 *) archive_header;
     int32 temp_asset_count = 0;
     archive_header += sizeof(uint32);
 
+    // Write the asset dependency in a similar way to the asset count
+    // The asset dependency describes how many other assets this dependency relies on
     *((uint32 *) archive_header) = 0;
     uint32* asset_dependency_count = (uint32 *) archive_header;
     archive_header += sizeof(uint32);
 
-    int64 index = 0;
-
-    // Create archive file data
+    // Create archive file data by loading the assets from the table of contents (toc)
+    // and then loading this asset data into our archive file
+    // While "copying" the asset data over to the asset archive we do some transformation & compression
+    // as needed and used in our game engine
     byte* pos = toc->content;
     while (*pos != '\0') {
-        // Get file path
+        // Get the file path for the asset (stored in the toc)
         char* file_path = (char *) pos;
         str_move_to((const char **) &pos, '\n');
         if (*pos == '\n') {
@@ -217,32 +227,39 @@ void build_asset_archive(RingMemory* memory_volatile, char* argv[], const char* 
         }
 
         // Make path absolute based on input file
-        char input_path[MAX_PATH];
+        char input_path[PATH_MAX_LENGTH];
         if (*file_path == '.') {
-            memcpy(input_path, rel_path, MAX_PATH);
-            strcpy(input_path + str_length(input_path), file_path + 1);
+            memcpy(input_path, rel_path, PATH_MAX_LENGTH);
+            strcpy(input_path + strlen(input_path), file_path + 1);
         } else {
             strcpy(input_path, file_path);
         }
 
-        // Get extension
+        if (!file_exists(input_path)) {
+            printf("Couldn't find %s\n", input_path);
+        }
+
+        // Get the file extension of the asset (we handle file types differently)
         char* extension = (char *) (pos - 1);
         while (*extension != '.') {
             --extension;
         }
 
+        // We need this to later on calculate the written size to the asset archive
         ++temp_asset_count;
 
         byte* element_start = archive_body;
-        uint32 uncompressed_length;
 
-        uint32 element_type;
+        // The raw data size (usefull, when loading the asset so we can reserve the correct size immediately)
+        uint32 uncompressed_length = 0;
+
+        uint32 element_type = ASSET_TYPE_GENERAL;
         if (strncmp(extension, ".wav", sizeof("wav") - 1) == 0) {
             element_type = ASSET_TYPE_AUDIO;
 
             // Read file
             Audio audio;
-            audio.data = ring_get_memory(memory_volatile, file_size(input_path) + sizeof(Audio), 4);
+            audio.data = ring_memory_get(memory_volatile, file_size(input_path) + sizeof(Audio), 4);
             audio_from_file(&audio, input_path, memory_volatile);
 
             // Create output data
@@ -255,7 +272,7 @@ void build_asset_archive(RingMemory* memory_volatile, char* argv[], const char* 
 
             // Read file
             Mesh mesh;
-            mesh.data = ring_get_memory(memory_volatile, file_size(input_path) * 2 + sizeof(Mesh), 4);
+            mesh.data = ring_memory_get(memory_volatile, file_size(input_path) * 2 + sizeof(Mesh), 4);
             mesh_from_file_txt(&mesh, input_path, memory_volatile);
 
             // Create output data
@@ -266,7 +283,7 @@ void build_asset_archive(RingMemory* memory_volatile, char* argv[], const char* 
 
             // Read file
             Language lang;
-            lang.data = ring_get_memory(memory_volatile, file_size(input_path) + 1024, 4);
+            lang.data = ring_memory_get(memory_volatile, file_size(input_path) + 1024, 4);
             language_from_file_txt(&lang, input_path, memory_volatile);
 
             // Create output data
@@ -277,7 +294,7 @@ void build_asset_archive(RingMemory* memory_volatile, char* argv[], const char* 
 
             // Read file
             Font font;
-            font.glyphs = (Glyph *) ring_get_memory(memory_volatile, file_size(input_path) + sizeof(Glyph), 4);
+            font.glyphs = (Glyph *) ring_memory_get(memory_volatile, file_size(input_path) + sizeof(Glyph), 4);
             font_from_file_txt(&font, input_path, memory_volatile);
 
             // Create output data
@@ -288,7 +305,7 @@ void build_asset_archive(RingMemory* memory_volatile, char* argv[], const char* 
 
             // Read file
             UIThemeStyle theme;
-            theme.data = ring_get_memory(memory_volatile, file_size(input_path) * 2 + sizeof(UIThemeStyle), 4);
+            theme.data = ring_memory_get(memory_volatile, file_size(input_path) * 2 + sizeof(UIThemeStyle), 4);
             theme_from_file_txt(&theme, input_path, memory_volatile);
 
             // Create output data
@@ -300,7 +317,7 @@ void build_asset_archive(RingMemory* memory_volatile, char* argv[], const char* 
         ) {
             element_type = ASSET_TYPE_IMAGE;
 
-            Image image = {};
+            Image image = {0};
 
             if (strncmp(extension, ".png", sizeof("png") - 1) == 0) {
                 // @todo Once we have implemented our own png just use the image_from_file function below
@@ -326,33 +343,40 @@ void build_asset_archive(RingMemory* memory_volatile, char* argv[], const char* 
             if (strncmp(extension, ".png", sizeof("png") - 1) == 0) {
                 free(image.pixels);
             }
+        } else if (strncmp(extension, ".fs", sizeof("fs") - 1) == 0
+            || strncmp(extension, ".vs", sizeof("vs") - 1) == 0
+            || strncmp(extension, ".hlsl", sizeof("hlsl") - 1) == 0
+        ) {
+            element_type = ASSET_TYPE_GENERAL;
+
+            FileBody file = {0};
+            file_read(input_path, &file, memory_volatile);
+
+            char* optimized = (char *) ring_memory_get(memory_volatile, file.size, 4);
+
+            int32 opt_size = strncmp(extension, ".hlsl", sizeof("hlsl") - 1)
+                ? directx_program_optimize((char *) file.content, optimized)
+                : opengl_program_optimize((char *) file.content, optimized);
+
+            // @todo we should compress this file here
+            memcpy(archive_body, optimized, opt_size);
+            uncompressed_length = opt_size;
+            archive_body += uncompressed_length;
         } else {
             element_type = ASSET_TYPE_GENERAL;
 
-            FileBody file = {};
+            FileBody file = {0};
             file_read(input_path, &file, memory_volatile);
 
-            if (strncmp(extension, ".fs", sizeof("fs") - 1) == 0
-                || strncmp(extension, ".vs", sizeof("vs") - 1) == 0
-                || strncmp(extension, ".hlsl", sizeof("hlsl") - 1) == 0
-            ) {
-                char* optimized = (char *) ring_get_memory(memory_volatile, file.size, 4);
-
-                int32 opt_size = strncmp(extension, ".hlsl", sizeof("hlsl") - 1)
-                    ? directx_program_optimize((char *) file.content, optimized)
-                    : opengl_program_optimize((char *) file.content, optimized);
-
-                memcpy(archive_body, optimized, opt_size);
-                uncompressed_length = opt_size;
-                archive_body += uncompressed_length;
-            } else {
-                memcpy(archive_body, file.content, file.size);
-                uncompressed_length = (uint32) file.size;
-                archive_body += uncompressed_length;
-            }
+            // @todo we should compress this file here
+            memcpy(archive_body, file.content, file.size);
+            uncompressed_length = (uint32) file.size;
+            archive_body += uncompressed_length;
         }
 
+        // Write element information to the header
         // WARNING: the offsets need to be adjusted in the future by the header size
+        //          We add the header size further below
 
         // Type
         *((uint32 *) archive_header) = SWAP_ENDIAN_LITTLE(element_type);
@@ -379,6 +403,7 @@ void build_asset_archive(RingMemory* memory_volatile, char* argv[], const char* 
         archive_header += sizeof(uint32);
     }
 
+    // Now we update some header information as mentioned in the beginning
     *asset_count = SWAP_ENDIAN_LITTLE(temp_asset_count);
     *asset_dependency_count = SWAP_ENDIAN_LITTLE(*asset_dependency_count);
 
@@ -397,10 +422,12 @@ void build_asset_archive(RingMemory* memory_volatile, char* argv[], const char* 
         archive_header += sizeof(AssetArchiveElement);
     }
 
-    // Output archive file
+    // We now write the data to the file
     file_write(argv[2], &output_header);
 
+    // At this place we also remove the unused space in the header
     output_body.size = archive_body - output_body.content;
+    // @todo this is not efficient, we create a file handle twice, we should re-use the fh from the file_write()
     file_append(argv[2], &output_body);
 }
 
@@ -413,11 +440,11 @@ int32 main(int32 argc, char* argv[])
 
     printf("Generating archive...\n");
 
-    char rel_path[MAX_PATH];
+    char rel_path[PATH_MAX_LENGTH];
     create_base_path(argv[1], rel_path);
 
     // Table of contents used to create the archive file
-    FileBody toc = {};
+    FileBody toc = {0};
     toc.content = (byte *) malloc(sizeof(byte) * MEGABYTE * 4);
     file_read(argv[1], &toc);
 
@@ -431,7 +458,7 @@ int32 main(int32 argc, char* argv[])
     ++toc.content;
     --toc.size;
 
-    RingMemory memory_volatile = {};
+    RingMemory memory_volatile = {0};
     memory_volatile.memory = (byte *) malloc(sizeof(byte) * GIGABYTE * 1);
     memory_volatile.head = memory_volatile.memory;
     memory_volatile.size = sizeof(byte) * GIGABYTE * 1;
