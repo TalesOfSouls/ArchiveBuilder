7cac92eef1685cdc9f2071c4c7b7e7f6552256f3
Dennis Eichhorn <spl1nes.com@googlemail.com>
2026-04-24 22:17:10 +0200

    back to working

--- archive_builder.cpp
+++ archive_builder.cpp
@@ -13,6 +13,7 @@
 #include "../cOMS/system/FileUtils.cpp"
 #include "../cOMS/ui/UITheme.cpp"
 #include "../cOMS/utils/StringUtils.h"
+#include "../cOMS/asset/AssetArchive.h"
 #include "../cOMS/asset/AssetArchive.cpp"
 #include "../cOMS/audio/Audio.cpp"
 #include "../cOMS/audio/Qoa.h"
@@ -47,7 +48,7 @@ void create_base_path(const char* path, char* rel_path)
 
 void enum_name_from_path(const char* path, char* enum_name) {
     // Extract the last directory and file name
-    char* last_dir = strrchr(path, '/');
+    const char* last_dir = strrchr(path, '/');
     if (!last_dir) {
         last_dir = strrchr(path, '\\');
     }
@@ -57,32 +58,32 @@ void enum_name_from_path(const char* path, char* enum_name) {
         return;
     }
 
-    char* file_name = last_dir + 1;
+    const char* file_name = last_dir + 1;
+    strcpy(enum_name, file_name);
 
     // Remove file extension
-    char* dot = strrchr(file_name, '.');
+    char* dot = strrchr(enum_name, '.');
     if (dot != NULL) {
         *dot = '_';
     }
 
     // If the file contains multiple . as file extension we ignore the last one
-    // The reason for this is that we sometimes need platform/api specific file names but in our enum we need the same name between all enums (e.g. compiled vulkan shaders)
-    char* dot2 = strrchr(file_name, '.');
+    // The reason for this is that we sometimes need platform/api specific file names
+    // but in our enum we need the same name between all enums (e.g. compiled vulkan shaders)
+    char* dot2 = strrchr(enum_name, '.');
     if (dot2 != NULL && last_dir < dot2) {
         *dot2 = '_';
         *dot = '\0';
     }
 
     // Transform to enum format
-    int32 j = 0;
-    for (int32 c = 0; file_name[c] != '\0'; ++c) {
-        if (isalnum(file_name[c])) {
-            enum_name[j++] = (char) toupper(file_name[c]);
+    for (int32 c = 0; enum_name[c] != '\0'; ++c) {
+        if (isalnum(enum_name[c])) {
+            enum_name[c] = (char) toupper(file_name[c]);
         } else {
-            enum_name[j++] = '_';
+            enum_name[c] = '_';
         }
     }
-    enum_name[j] = '\0';
 };
 
 void build_enum(RingMemory* memory_volatile, const char* enum_char, int32 asset_count, char* output_path, int32 toc_id)
@@ -127,7 +128,7 @@ void build_enum(RingMemory* memory_volatile, const char* enum_char, int32 asset_
         "#ifndef TOS_ASSET_ARCHIVE_IDS_%s\n"
         "#define TOS_ASSET_ARCHIVE_IDS_%s\n"
         "\n"
-        "enum AssetArchiveIds%s {\n",
+        "enum AssetArchiveIds%s : int32 {\n",
         namespace_name,
         namespace_name,
         output_name
@@ -161,33 +162,38 @@ void build_enum(RingMemory* memory_volatile, const char* enum_char, int32 asset_
     file_write(output_path, &enum_file);
 }
 
-build_asset(
+void build_asset(
     RingMemory* memory_volatile,
     const char* rel_path,
     const char* input_path,
-    const char** archive_body, const char* body_start,
+    byte** archive_body, const byte* body_start,
     char* enum_char,
     AssetArchiveHeader* header,
     int32 archive_id
 ) {
-    if (!file_exists(input_path)) {
-        printf("Couldn't find %s\n", input_path);
+    char abs_input_path[PATH_MAX_LENGTH] = {0};
+    if (*input_path == '.') {
+        memcpy(abs_input_path, rel_path, PATH_MAX_LENGTH);
+        strcpy(abs_input_path + strlen(abs_input_path), input_path + 1);
+    } else {
+        strcpy(abs_input_path, input_path);
+    }
+
+    if (!file_exists(abs_input_path)) {
+        printf("Couldn't find %s\n", abs_input_path);
         return;
     }
 
-    enum_name_from_path(input_path, &enum_char[64 * header->asset_count]);
+    enum_name_from_path(abs_input_path, &enum_char[64 * header->asset_count]);
 
     // Get the file extension of the asset (we handle file types differently)
-    const char* extension = (input_path + 1);
-    while (*extension != '.') {
-        --extension;
-    }
+    const char* extension = strrchr(abs_input_path, '.');
 
     byte* element_start = *archive_body;
 
     AssetArchiveElement* element = &header->asset_element[header->asset_count];
     element->type = ASSET_TYPE_GENERAL;
-    element->start = (uint32) (archive_body - body_start);
+    element->start = (uint32) ((uintptr_t) *archive_body - (uintptr_t) body_start);
 
     ++header->asset_count;
 
@@ -196,8 +202,8 @@ build_asset(
 
         // Read file
         Audio audio;
-        audio.data = ring_memory_get(memory_volatile, file_size(input_path) + sizeof(Audio), 4);
-        audio_from_file(&audio, input_path, memory_volatile);
+        audio.data = ring_memory_get(memory_volatile, file_size(abs_input_path) + sizeof(Audio), 4);
+        audio_from_file(&audio, abs_input_path, memory_volatile);
 
         // Create output data
         element->uncompressed = audio_data_size(&audio);
@@ -205,68 +211,76 @@ build_asset(
         *archive_body += audio_header_to_data(&audio, *archive_body);
         *archive_body += qoa_encode(&audio, *archive_body);
 
-        element->length = *archive_body - element_start;
+        element->length = (uint32) ((uintptr_t) *archive_body - (uintptr_t) element_start);
     } else if (strncmp(extension, ".objtxt", sizeof("objtxt") - 1) == 0) {
         element->type = ASSET_TYPE_OBJ;
 
         // Read file
         Mesh mesh;
-        mesh.data = ring_memory_get(memory_volatile, file_size(input_path) * 2 + sizeof(Mesh), 4);
-        mesh_from_file_txt(&mesh, input_path, memory_volatile);
+        mesh.data = ring_memory_get(memory_volatile, file_size(abs_input_path) * 2 + sizeof(Mesh), 4);
+        mesh_from_file_txt(&mesh, abs_input_path, memory_volatile);
 
         // Create output data
         element->uncompressed = mesh_to_data(&mesh, *archive_body);
         *archive_body += element->uncompressed;
 
-        element->length = *archive_body - element_start;
+        element->length = (uint32) ((uintptr_t) *archive_body - (uintptr_t) element_start);
     } else if (strncmp(extension, ".langtxt", sizeof("langtxt") - 1) == 0) {
         element->type = ASSET_TYPE_LANGUAGE;
 
         // Read file
         Language lang;
-        lang.data = ring_memory_get(memory_volatile, file_size(input_path) + 1024, 4);
-        language_from_file_txt(&lang, input_path, memory_volatile);
+        lang.data = ring_memory_get(memory_volatile, file_size(abs_input_path) + 1024, 4);
+        language_from_file_txt(&lang, abs_input_path, memory_volatile);
 
         // Create output data
         element->uncompressed = language_to_data(&lang, *archive_body);
         *archive_body += element->uncompressed;
 
-        element->length = *archive_body - element_start;
+        element->length = (uint32) ((uintptr_t) *archive_body - (uintptr_t) element_start);
     } else if (strncmp(extension, ".atlastxt", sizeof("atlastxt") - 1) == 0) {
         element->type = ASSET_TYPE_TEXTURE_ATLAS;
 
         // Read file
         TextureAtlas atlas = {0};
-        atlas_from_file_txt(&atlas, input_path, memory_volatile);
+        atlas_from_file_txt(&atlas, abs_input_path, memory_volatile);
 
         // Create output data
         element->uncompressed = atlas_to_data(&atlas, *archive_body);
         *archive_body += element->uncompressed;
 
-        element->length = *archive_body - element_start;
+        element->length = (uint32) ((uintptr_t) *archive_body - (uintptr_t) element_start);
 
+        // @question Consider to pull out
         bool is_external = str_is_num(atlas.texture_name);
         int32 texture_id;
 
+        int32 dependency_index = element->dependency_count++;
+        element->dependency_start = header->asset_dependency_count++;
+
         if (is_external) {
-            texture_id = str_to_int(atlas.texture_name);
+            texture_id = (int32) str_to_int(atlas.texture_name);
         } else {
-            wchar_t dependency_input_path[MAX_PATH];
+            char dependency_input_path[MAX_PATH];
             int32 i = 0;
 
             if (atlas.texture_name[0] != '.') {
-                memcpy(dependency_input_path, L"./", sizeof(L"./"));
+                memcpy(dependency_input_path, "./", sizeof("./"));
                 i = 2;
             }
 
             for (const char* pos = atlas.texture_name; *pos; ++pos) {
-                dependency_input_path[i++] = (wchar_t) *pos;
+                dependency_input_path[i++] = (char) *pos;
             }
 
+            dependency_input_path[i] = '\0';
+
             texture_id = (header->asset_count) | (archive_id << 24);
+            char new_rel_path[MAX_PATH] = {0};
+            create_base_path(abs_input_path, new_rel_path);
             build_asset(
                 memory_volatile,
-                rel_path,
+                new_rel_path,
                 dependency_input_path,
                 archive_body, body_start,
                 enum_char,
@@ -274,45 +288,51 @@ build_asset(
                 archive_id
             );
         }
-        element->dependency_start = header->asset_dependency_count;
-        header->asset_dependencies[element->dependency_count++] = texture_id;
-        ++header->asset_dependency_count;
+        header->asset_dependencies[element->dependency_start + dependency_index] = texture_id;
     } else if (strncmp(extension, ".fonttxt", sizeof("fonttxt") - 1) == 0) {
         element->type = ASSET_TYPE_FONT;
 
         // Read file
         Font font = {0};
-        font.glyphs = (Glyph *) ring_memory_get(memory_volatile, file_size(input_path) + sizeof(Glyph), 4);
-        font_from_file_txt(&font, input_path, memory_volatile);
+        font.glyphs = (Glyph *) ring_memory_get(memory_volatile, file_size(abs_input_path) + sizeof(Glyph), 4);
+        font_from_file_txt(&font, abs_input_path, memory_volatile);
 
         // Create output data
         element->uncompressed = font_to_data(&font, *archive_body);
         *archive_body += element->uncompressed;
 
-        element->length = *archive_body - element_start;
+        element->length = (uint32) ((uintptr_t) *archive_body - (uintptr_t) element_start);
 
+        // @question Consider to pull out
         bool is_external = str_is_num(font.texture_name);
         int32 texture_id;
 
+        int32 dependency_index = element->dependency_count++;
+        element->dependency_start = header->asset_dependency_count++;
+
         if (is_external) {
-            texture_id = str_to_int(font.texture_name);
+            texture_id = (int32) str_to_int(font.texture_name);
         } else {
-            wchar_t dependency_input_path[MAX_PATH];
+            char dependency_input_path[MAX_PATH];
             int32 i = 0;
 
             if (font.texture_name[0] != '.') {
-                memcpy(dependency_input_path, L"./", sizeof(L"./"));
+                memcpy(dependency_input_path, "./", sizeof("./"));
                 i = 2;
             }
 
             for (const char* pos = font.texture_name; *pos; ++pos) {
-                dependency_input_path[i++] = (wchar_t) *pos;
+                dependency_input_path[i++] = (char) *pos;
             }
 
+            dependency_input_path[i] = '\0';
+
             texture_id = (header->asset_count) | (archive_id << 24);
+            char new_rel_path[MAX_PATH] = {0};
+            create_base_path(abs_input_path, new_rel_path);
             build_asset(
                 memory_volatile,
-                rel_path,
+                new_rel_path,
                 dependency_input_path,
                 archive_body, body_start,
                 enum_char,
@@ -320,22 +340,20 @@ build_asset(
                 archive_id
             );
         }
-        element->dependency_start = header->asset_dependency_count;
-        header->asset_dependencies[element->dependency_count++] = texture_id;
-        ++header->asset_dependency_count;
+        header->asset_dependencies[element->dependency_start + dependency_index] = texture_id;
     } else if (strncmp(extension, ".themetxt", sizeof("themetxt") - 1) == 0) {
         element->type = ASSET_TYPE_THEME;
 
         // Read file
         UIThemeStyle theme;
-        theme.data = ring_memory_get(memory_volatile, file_size(input_path) * 2 + sizeof(UIThemeStyle), 4);
-        theme_from_file_txt(&theme, input_path, memory_volatile);
+        theme.data = ring_memory_get(memory_volatile, file_size(abs_input_path) * 2 + sizeof(UIThemeStyle), 4);
+        theme_from_file_txt(&theme, abs_input_path, memory_volatile);
 
         // Create output data
         element->uncompressed = theme_to_data(&theme, *archive_body);
         *archive_body += element->uncompressed;
 
-        element->length = *archive_body - element_start;
+        element->length = (uint32) ((uintptr_t) *archive_body - (uintptr_t) element_start);
     } else if (strncmp(extension, ".png", sizeof("png") - 1) == 0
         || strncmp(extension, ".bmp", sizeof("bmp") - 1) == 0
         || strncmp(extension, ".tga", sizeof("tga") - 1) == 0
@@ -347,7 +365,7 @@ build_asset(
         if (strncmp(extension, ".png", sizeof("png") - 1) == 0) {
             // @todo Once we have implemented our own png just use the image_from_file function below
             int32 w2,h2;
-            byte* data2 = stbi_load(input_path, &w2, &h2, 0, 4);
+            byte* data2 = stbi_load(abs_input_path, &w2, &h2, 0, 4);
 
             image.width = w2;
             image.height = h2;
@@ -355,7 +373,7 @@ build_asset(
             image.pixels = data2;
             image.image_settings |= 4;
         } else {
-            image_from_file(&image, input_path, memory_volatile);
+            image_from_file(&image, abs_input_path, memory_volatile);
         }
 
         // @performance The way how we load assets (see overhead usage) we could maybe use only the pixel data as size instead of also including the header size here
@@ -369,7 +387,7 @@ build_asset(
             free(image.pixels);
         }
 
-        element->length = *archive_body - element_start;
+        element->length = (uint32) ((uintptr_t) *archive_body - (uintptr_t) element_start);
     } else if (strncmp(extension, ".fs", sizeof("fs") - 1) == 0
         || strncmp(extension, ".vs", sizeof("vs") - 1) == 0
         || strncmp(extension, ".hlsl", sizeof("hlsl") - 1) == 0
@@ -377,7 +395,7 @@ build_asset(
         element->type = ASSET_TYPE_GENERAL;
 
         FileBody file = {0};
-        file_read(input_path, &file, memory_volatile);
+        file_read(abs_input_path, &file, memory_volatile);
 
         char* optimized = (char *) ring_memory_get(memory_volatile, file.size, 4);
 
@@ -390,19 +408,19 @@ build_asset(
         element->uncompressed = opt_size;
         *archive_body += element->uncompressed;
 
-        element->length = *archive_body - element_start;
+        element->length = (uint32) ((uintptr_t) *archive_body - (uintptr_t) element_start);
     } else {
         element->type = ASSET_TYPE_GENERAL;
 
         FileBody file = {0};
-        file_read(input_path, &file, memory_volatile);
+        file_read(abs_input_path, &file, memory_volatile);
 
         // @todo we should compress this file here
         memcpy(*archive_body, file.content, file.size);
         element->uncompressed = (uint32) file.size;
         *archive_body += element->uncompressed;
 
-        element->length = *archive_body - element_start;
+        element->length = (uint32) ((uintptr_t) *archive_body - (uintptr_t) element_start);
     }
 }
 
@@ -422,9 +440,9 @@ int32 build_asset_archive(
     memory_output.end = memory_output.memory + memory_output.size;
 
     AssetArchiveHeader header = {0};
-    header->version = ASSET_ARCHIVE_VERSION;
-    header->asset_element = (AssetArchiveElement *) malloc(sizeof(AssetArchiveElement) * 10000);
-    header->asset_dependencies = (uint32 *) malloc(sizeof(uint32) * 10000);
+    header.version = ASSET_ARCHIVE_VERSION;
+    header.asset_element = (AssetArchiveElement *) malloc(sizeof(AssetArchiveElement) * 10000);
+    header.asset_dependencies = (uint32 *) malloc(sizeof(uint32) * 10000);
 
     // We don't know how large our header actually needs to be
     // We just pick a generous offset.
@@ -479,36 +497,37 @@ int32 build_asset_archive(
     output = write_le(output, header.asset_count);
     output = write_le(output, header.asset_dependency_count);
 
-    const int32 header_offset = sizeof(archive->header.version)
-        + sizeof(archive->header.asset_count)
-        + sizeof(archive->header.asset_dependency_count)
-        + header->asst_count * sizeof(AssetArchiveElement)
-        + header->asset_dependency_count * sizeof(int32);
+    const int32 header_offset = (int32) (sizeof(header.version)
+        + sizeof(header.asset_count)
+        + sizeof(header.asset_dependency_count)
+        + header.asset_count * sizeof(AssetArchiveElement)
+        + header.asset_dependency_count * sizeof(int32))
+        - header.asset_element[0].start;
 
     for (int i = 0; i < header.asset_count; ++i) {
-        header.asset_element->start += header_offset;
+        header.asset_element[i].start += header_offset;
     }
 
     memcpy(output, header.asset_element, header.asset_count * sizeof(AssetArchiveElement));
     SWAP_ENDIAN_LITTLE_SIMD(
         (int32 *) output,
         (int32 *) output,
-        header->asset_count * sizeof(AssetArchiveElement) / 4, // everything is 4 bytes -> easy to swap
+        header.asset_count * sizeof(AssetArchiveElement) / 4, // everything is 4 bytes -> easy to swap
         1
     );
     output += header.asset_count * sizeof(AssetArchiveElement);
 
     // Now write dependency array
-    if (header->asset_dependency_count > 0) {
+    if (header.asset_dependency_count > 0) {
         ASSERT_TRUE(output % alignof(uint32));
-        memcpy(output, header->asset_dependencies, header->asset_dependency_count * sizeof(uint32));
+        memcpy(output, header.asset_dependencies, header.asset_dependency_count * sizeof(uint32));
         SWAP_ENDIAN_LITTLE_SIMD(
             (int32 *) output,
             (int32 *) output,
-            header->asset_dependency_count, // everything is 4 bytes -> easy to swap
+            header.asset_dependency_count, // everything is 4 bytes -> easy to swap
             1
         );
-        output += header->asset_dependency_count * sizeof(uint32);
+        output += header.asset_dependency_count * sizeof(uint32);
     }
 
     memcpy(output, body_start, archive_body - body_start);
@@ -518,6 +537,8 @@ int32 build_asset_archive(
 
     // We now write the data to the file
     file_write(argv[2], &output_body);
+
+    return header.asset_count;
 }
 
 int32 main(int32 argc, char* argv[])
@@ -554,7 +575,7 @@ int32 main(int32 argc, char* argv[])
     memory_volatile.end = memory_volatile.memory + memory_volatile.size;
 
     // This holds all the enum names
-    char* enum_char = malloc(sizeof(char) * 64 * 1024);
+    char* enum_char = (char *) malloc(sizeof(char) * 64 * 1024);
 
     // Careful, argv and toc both get modified.
     int32 asset_count = build_asset_archive(&memory_volatile, argv, rel_path, enum_char, &toc, toc_id);
